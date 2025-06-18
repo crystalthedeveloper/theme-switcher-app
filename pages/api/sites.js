4a79c09eaccaf6c577cde06a487dc278ed4385e2
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2025-06-18 10:23:17 -0400

    commit

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // Handle preflight
+  // Handle the preflight
   if (req.method === "OPTIONS") return res.status(204).end();
 
   // Only allow POST
