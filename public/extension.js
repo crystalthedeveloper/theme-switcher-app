b6c3e692f25eab990ba535a363117b6f780c8adf
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2025-06-19 17:29:14 -0400

    feat: add Webflow API version switch logic to inject-footer endpoint

---
+++ public/extension.js
@@ -0,0 +1,7 @@
+// /public/extension.js
+Webflow.require('ix2').init();
+
+const script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/gh/crystalthedeveloper/theme-switcher/theme-switcher.js';
+script.defer = true;
+document.body.appendChild(script);
