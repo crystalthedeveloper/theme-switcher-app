967fd515f44f95802a5744f77dd8df2e7d72990a
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2025-06-20 05:04:50 -0400

    feat: add images

--- public/extension.js
+++ public/extension.js
@@ -5,3 +5,7 @@ const script = document.createElement('script');
 script.src = 'https://cdn.jsdelivr.net/gh/crystalthedeveloper/theme-switcher/theme-switcher.js';
 script.defer = true;
 document.body.appendChild(script);
+
+script.onerror = () => {
+  console.error('⚠️ Theme Switcher script failed to load.');
+};
